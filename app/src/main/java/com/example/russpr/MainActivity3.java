f3121fcbf17fcc8319df13c35451b6be693a4d77
33Kl <74059835+33Kl@users.noreply.github.com>
2021-04-10 22:24:11 +0300

    Updated `MainActivity` files #3 and #5

--- app/src/main/java/com/example/russpr/MainActivity3.java
+++ app/src/main/java/com/example/russpr/MainActivity3.java
@@ -12,6 +12,16 @@ public class MainActivity3 extends AppCompatActivity implements OnClickListener{
 
     private Button ex9;
     private Button ex10;
+    private Button ex11;
+    private Button ex12;
+    private Button ex13;
+    private Button ex14;
+    private Button ex15;
+    private Button ex16;
+    private Button ex17;
+    private Button ex18;
+    private Button ex19;
+    private Button ex20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,19 +32,36 @@ public class MainActivity3 extends AppCompatActivity implements OnClickListener{
         ex9.setOnClickListener(this);
         ex10 = (Button)findViewById(R.id.ex10);
         ex10.setOnClickListener(this);
+        ex11 = (Button)findViewById(R.id.ex11);
+        ex11.setOnClickListener(this);
+        ex12 = (Button)findViewById(R.id.ex12);
+        ex12.setOnClickListener(this);
+        ex13 = (Button)findViewById(R.id.ex13);
+        ex13.setOnClickListener(this);
+        ex14 = (Button)findViewById(R.id.ex14);
+        ex14.setOnClickListener(this);
+        ex15 = (Button)findViewById(R.id.ex15);
+        ex15.setOnClickListener(this);
+        ex16 = (Button)findViewById(R.id.ex16);
+        ex16.setOnClickListener(this);
+        ex17 = (Button)findViewById(R.id.ex17);
+        ex17.setOnClickListener(this);
+        ex18 = (Button)findViewById(R.id.ex18);
+        ex18.setOnClickListener(this);
+        ex19 = (Button)findViewById(R.id.ex19);
+        ex19.setOnClickListener(this);
+        ex20 = (Button)findViewById(R.id.ex20);
+        ex20.setOnClickListener(this);
 
     }
     @Override
     public void onClick(View v){
         switch (v.getId()){
             case R.id.ex9:
-                Intent intent5 = new Intent (this,MainActivity5.class);
-                startActivity(intent5);
+
                 break;
             case R.id.ex10:
-                Intent intent6  = new Intent(this,MainActivity5.class);
-                startActivity(intent6);
-                break;
+
         }
     }
 }
